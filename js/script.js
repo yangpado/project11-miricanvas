f1972862e1c86d8a49830c8382adc8601192ce25
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-01 23:21:45 +0900

    JS - swiper ing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 $(function(){
+	/*swiper*/
+	var swiper = new Swiper('.swiper-container',{
+		sliderPerView:4, //화면에 표시되는 슬라이드 갯수
+		spaceBetween:30, //슬라이드 간격
+		autoplay:{ //자동재생
+			delay:5000, //정지시간 
+		},
+		breakpoints:{ //반응형중단점
+		600:{
+			slidesPerView:1.4,
+			spaceBetween:24,
+		}
+		768:{
+			slidesPerView:2,
+			spaceBetween:24,
+		}
+		960:{
+			slidesPerView:3,
+			spaceBetween:24,
+		}
+		
+		}
+		
+	})
 	/*bx slider*/
 	$('.bxslider').bxSlider({
 		mode:'horizontal', //슬라이드 이동 방향 설정
